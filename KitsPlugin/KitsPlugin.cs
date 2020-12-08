da0c5fc56991dfc5af417b24ab8c32dcf515832b
RestoreMonarchy <35507241+RestoreMonarchy@users.noreply.github.com>
2020-12-08 16:51:21 +0100

    Add project files.

---
+++ KitsPlugin/KitsPlugin.cs
@@ -0,0 +1,25 @@
+﻿using Rocket.Core.Logging;
+using Rocket.Core.Plugins;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KitsPlugin
+{
+    public class KitsPlugin : RocketPlugin<KitsPluginConfiguration>
+    {
+
+        protected override void Load()
+        {
+            Logger.Log(Configuration.Instance.LoadMessage);
+            Logger.Log($"{Name} {Assembly.GetName().Version} has been loaded!");
+        }
+
+        protected override void Unload()
+        {
+            Logger.Log($"{Name} has been unloaded!");
+        }
+    }
+}
---
+++ KitsPlugin/KitsPluginConfiguration.cs
@@ -0,0 +1,19 @@
+﻿using Rocket.API;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KitsPlugin
+{
+    public class KitsPluginConfiguration : IRocketPluginConfiguration
+    {
+        public string LoadMessage { get; set; }
+
+        public void LoadDefaults()
+        {
+            LoadMessage = "This is kits plugin!";
+        }
+    }
+}
